c082c5ca591d36bd5d0ef201de0bd95af8b83f09
Ruslan <ruslanri@code.edu.az>
2022-09-27 20:14:14 +0400

    Lahiye

--- Lahiye/Service/Interface/IBranchService.cs
+++ Lahiye/Service/Interface/IBranchService.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Lahiye.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Lahiye.Service.Interface
 {
-    internal interface IBranchService
+    public  interface IBranchService:IBankService<Branch>
     {
+        void HireEmployee();
+        void GetProfit();
+        void TransferMoney();
+        void TransferEmployee();
     }
 }
